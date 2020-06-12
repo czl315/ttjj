5c25f2ffed8205581e0d6b6e993863d471ec48e8
chenzhilong9 <chenzhilong9@jd.com>
2020-06-12 14:09:15 +0800

    主题排行

--- src/main/java/hy/BizRank.java
+++ src/main/java/hy/BizRank.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 主题排行-每日
+ * 主题排行
  */
 public class BizRank {
 
@@ -20,9 +20,33 @@ public class BizRank {
      */
     public static void main(String[] args) {
         int endCount = 10;//显示数量
+
 //        String findDateType ="SYL_D";//查询类型 SYL_D:日；SYL_Z：周；SYL_Y：月；SYL_3Y：季；SYL_6Y：半年；SYL_1N：1年；SYL_JN：今年；
 //        String findDateType ="SYL_Z";//查询类型 SYL_D:日；SYL_Z：周；SYL_Y：月；SYL_3Y：季；SYL_6Y：半年；SYL_1N：1年；SYL_JN：今年；
         String findDateType ="SYL_D";//查询类型 SYL_D:日；SYL_Z：周；SYL_Y：月；SYL_3Y：季；SYL_6Y：半年；SYL_1N：1年；SYL_JN：今年；
+        System.out.println("查询主题排名-日:");
+        findTtjjTopicByYesterday(findDateType,endCount);//查询主题排名by时间类型、显示个数
+        System.out.println("查询主题排名-周:");
+        findTtjjTopicByYesterday("SYL_Z",endCount);//查询主题排名by时间类型、显示个数
+        System.out.println("查询主题排名-月:");
+        findTtjjTopicByYesterday("SYL_Y",endCount);//查询主题排名by时间类型、显示个数
+        System.out.println("查询主题排名-季:");
+        findTtjjTopicByYesterday("SYL_3Y",endCount);//查询主题排名by时间类型、显示个数
+        System.out.println("查询主题排名-半年:");
+        findTtjjTopicByYesterday("SYL_6Y",endCount);//查询主题排名by时间类型、显示个数
+        System.out.println("查询主题排名-1年:");
+        findTtjjTopicByYesterday("SYL_1N",endCount);//查询主题排名by时间类型、显示个数
+        System.out.println("查询主题排名-今年:");
+        findTtjjTopicByYesterday("SYL_JN",endCount);//查询主题排名by时间类型、显示个数
+
+    }
+
+    /**
+     * 查询昨日主题排名
+     * @param findDateType
+     * @param endCount
+     */
+    private static void findTtjjTopicByYesterday(String findDateType, int endCount) {
         //查询日  ：https://fundztapi.eastmoney.com/fundspecialapi/fundspecialfundtopiclist.ashx?callback=jQuery31107334621007101361_1591854447815&callbackname=fundData&sort=SYL_D&sorttype=desc&pageindex=1&pagesize=500&dt=11&tt=0&rs=WRANK&_=1591854447818
         //查询周  ：https://fundztapi.eastmoney.com/fundspecialapi/fundspecialfundtopiclist.ashx?callback=jQuery3110927108387611935_1591854756830 &callbackname=fundData&sort=SYL_Z&sorttype=desc&pageindex=1&pagesize=500&dt=11&tt=0&rs=WRANK&_=1591854756832
         String url = "https://fundztapi.eastmoney.com/fundspecialapi/fundspecialfundtopiclist.ashx";
