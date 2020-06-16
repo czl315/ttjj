4cb9edb016a73bf733e5a5dbc9dc0830758aa4d0
chenzhilong9 <chenzhilong9@jd.com>
2020-06-16 14:04:09 +0800

    work：新品季

--- src/main/java/work/NewSeason.java
+++ src/main/java/work/NewSeason.java
@@ -24,22 +24,7 @@ public class NewSeason extends TimerTask {
      * @param args args
      */
     public static void main(String[] args) {
-//        doBizTask();
-//        Timer timer = new Timer();
-//        NewSeason newSeason = new NewSeason();
-//        timer.schedule(newSeason,1000L,10000);
-
-        ScheduledExecutorService service = new ScheduledThreadPoolExecutor(1);
-
-        // 初始化延迟0ms开始执行，每隔200ms重新执行一次任务。
-        ScheduledExecutorService  pool = new ScheduledThreadPoolExecutor(1);
-        pool.scheduleAtFixedRate(new Runnable() {
-            public void run() {
-                // task to run goes here
-                System.out.println("Hello !");
-//                doBizTask();
-            }
-        }, 0, 200L, TimeUnit.MILLISECONDS);
+        doBizTask();
     }
 
     /**
@@ -47,7 +32,7 @@ public class NewSeason extends TimerTask {
      */
     private static void doBizTask() {
 //        System.out.println("查询开始：");
-        String cookie = "shshshfpa=7d458cc5-ef69-8cca-04f9-04e8eefb8031-1577076121; __jdu=15770761202431945958964; pinId=xcWTturruQA; shshshfpb=yjlcj3AiuwWhpqP6phZkXFA%3D%3D; __jdv=76161171|direct|-|none|-|1591945299888; areaId=1; ipLoc-djd=1-2809-0-0; shshshfp=4154bf8474fadcecaae419f9c9058aff; pin=czl315; unick=czl315; _tp=EOpsxLedIh%2Bha%2FKPkDpqCg%3D%3D; _pst=czl315; user-key=b33f3bbe-c314-4321-9cca-3b828ff5dc79; cn=49; TrackID=1j5aNi9FXnnxPIiOVZWH-bHo1BeIWq5lqynywK5V7NnJfmlwsJY8Pm81L6Twh3TFljY2Fp7XvNp3mo1ny1IWq2m1uef0T3v41awFzSZ6DSxI; thor=A718E266D9B2378CAFBC9387160E875110CE55110EA8365AD1E3F4FE2A9A392D6A6D90F36223F77C58A511CB434DF650A8896DFE2B28A43496006C991F0348884AD312738877666672FF9EA3B51C98FE8F2FA141799EEA8954AA9EFBDCE4394D9110DD945FF7D1FD0DA40C7326F24AE2C8472C8903A552E75468F6828D47682E; ceshi3.com=201; shshshsID=69438b643a25a11b9c107a1702852c34_1_1592184039515; __jda=122270672.15770761202431945958964.1577076120.1591945300.1592184030.6; __jdc=122270672; 3AB9D23F7A4B3C9B=PMRCFLCUO2QEAKTO3OLBOKLRFTIBQFWR423GYIVNGBMDU3YZKT53RRFSK4TL72V5IF5FRF24EEQKY5ANIUW7RDPGPU; __jdb=122270672.7.15770761202431945958964|6.1592184030";
+        String cookie = "shshshfpa=7d458cc5-ef69-8cca-04f9-04e8eefb8031-1577076121; __jdu=15770761202431945958964; pinId=xcWTturruQA; shshshfpb=yjlcj3AiuwWhpqP6phZkXFA%3D%3D; __jdv=76161171|direct|-|none|-|1591945299888; areaId=1; ipLoc-djd=1-2809-0-0; shshshfp=4154bf8474fadcecaae419f9c9058aff; pin=czl315; unick=czl315; _tp=EOpsxLedIh%2Bha%2FKPkDpqCg%3D%3D; _pst=czl315; user-key=b33f3bbe-c314-4321-9cca-3b828ff5dc79; cn=49; TrackID=1j5aNi9FXnnxPIiOVZWH-bHo1BeIWq5lqynywK5V7NnJfmlwsJY8Pm81L6Twh3TFljY2Fp7XvNp3mo1ny1IWq2m1uef0T3v41awFzSZ6DSxI; thor=A718E266D9B2378CAFBC9387160E875157AD0BD2DA2709ED12F0EA893CAC173D030518677D0EA2D1EFAB1C09433182E8071B2C1D79C1BB91DE2FCBE525CB00B029DBE6D418524C1D79292E177E7E0C8D2B0C2F9F8B788D107F8725CAE6298E33672B188DD9E944ED04D9EFDCD127D6F0B99BB8732058D903843742F8A4E4E139; ceshi3.com=201; shshshsID=1f45537b4b610af7acabedf7cdc3435c_2_1592271504058; __jda=122270672.15770761202431945958964.1577076120.1592187280.1592271468.8; __jdc=122270672; 3AB9D23F7A4B3C9B=PMRCFLCUO2QEAKTO3OLBOKLRFTIBQFWR42";
 
 
         String browseType = "";
