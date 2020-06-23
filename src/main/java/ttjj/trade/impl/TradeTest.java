d2c922825203574b34c6dd759e5716e347b88e28
chenzhilong9 <chenzhilong9@jd.com>
2020-06-23 14:57:20 +0800

    ttjj

--- src/main/java/ttjj/trade/impl/TradeTest.java
+++ src/main/java/ttjj/trade/impl/TradeTest.java
@@ -2,22 +2,39 @@ package ttjj.trade.impl;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class TradeTest {
     public static void main(String[] args) {
-        Map<String,String> tradeRs=  new TradeServiceImpl().findFundTrade("000656");
-//        for (String trade : tradeRs.keySet()) {
-//            System.out.println(tradeRs.get(trade));
-//        }
-        if(tradeRs!=null){
-            System.out.println(JSON.toJSONString(tradeRs));
+        List<Map<String,String>> myHoldFundList = new ArrayList<Map<String,String>>();
+
+        List<String> fundCodeList = new ArrayList<String>();
+        fundCodeList.add("000656");
+        fundCodeList.add("001027");
+        fundCodeList.add("001279");
+        fundCodeList.add("001875");
+        fundCodeList.add("001915");
+
+
+        for (String fundCode : fundCodeList) {
+            Map<String,String> tradeRs=  new TradeServiceImpl().findFundTrade(fundCode);
+            myHoldFundList.add(tradeRs);
+        }
+        for (Map<String, String> tradeRs : myHoldFundList) {
+            if(tradeRs!=null){
+                System.out.println(JSON.toJSONString(tradeRs));
 //            System.out.println(tradeRs.get(SHARE_SUM));
 //            System.out.println(tradeRs.get(AMT_SUM));
 //            System.out.println(tradeRs.get(SERVER_CHARGE));
 //            System.out.println(tradeRs.get(FUND_NAME));
 //            System.out.println(tradeRs.get(FUND_CODE));
+            }
         }
+
+
+
 
     }
 }
