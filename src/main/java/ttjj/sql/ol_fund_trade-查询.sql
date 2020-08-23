b808297fb3d7946e7562c64b0a6ec629ab5e53c9
Administrator <czl315@163.com>
2020-08-24 00:27:24 +0800

    sql

--- src/main/java/ttjj/sql/ol_fund_trade-查询.sql
+++ src/main/java/ttjj/sql/ol_fund_trade-查询.sql
@@ -1,20 +1,22 @@
 /**基金交易的最新净值-收益率**/
 SELECT
 	ol_fund_trade.FD_INFO AS 最新收益倒序
-	,ol_fund_trade.TRADE_TIME
-	,ol_fund_trade.CONFIRM_SHARE
+
+	,ol_fund_trade.CONFIRM_SHARE AS 买入份额
 	,ROUND(
 		(ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100,
 		4
-	) AS 最新日期收益率
+	) AS 最新收益率
 ,ROUND((ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100/DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) ,4)
 		AS 每日收益率
-	,ol_fund_trade.CONFIRM_AMT
+	,ol_fund_trade.CONFIRM_AMT AS 交易金额
+,DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) AS 持有天数
+	,ol_fund_trade.TRADE_TIME
 	,ROUND(ol_fund_trade.LAST_NET * ol_fund_trade.CONFIRM_SHARE ,2) AS '最新金额'
 	,ol_fund_trade.CONFIRM_NET AS confirmNet
 	,ol_fund_trade.LAST_NET lastNet
 	-- 	(SELECT t1.LASTEST_NET FROM `ol_fund_earn` t1 WHERE t1.`FD_NAME` = ol_fund_trade.FD_INFO ORDER BY t1.`LASTEST_NET_DATA` DESC LIMIT 1) - ol_fund_trade.CONFIRM_NET AS syAmt,
-,DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) AS 现今持有天数
+
 ,ol_fund_trade.TYPE
 FROM
 	`ol_fund_trade` ol_fund_trade
@@ -23,15 +25,21 @@ WHERE
 	1 = 1
 	AND ol_fund_trade.TRADE_TIME>='2020-01-01 00:00:00'
 	AND ol_fund_trade.TYPE = '申购'
+-- AND ol_fund_trade.FD_INFO LIKE '%医%'
+-- AND ol_fund_trade.FD_INFO LIKE '%创业%'
+-- AND ol_fund_trade.FD_INFO LIKE '%军%'
+-- AND ol_fund_trade.FD_INFO LIKE '%消费%'
+-- AND ol_fund_trade.FD_INFO LIKE '%农业%'
+AND ol_fund_trade.FD_INFO LIKE '%金%'
 -- 	AND ol_fund_trade.CONFIRM_AMT <1000
 -- 	AND ol_fund_trade.TYPE in( '申购(赎回)' )
 -- 	AND ol_fund_trade.TYPE = '赎回'
 -- AND ol_fund_trade.FD_INFO LIKE '%160633|鹏华证券分级%'
--- ORDER BY lastDate DESC
+-- ORDER BY ol_fund_trade.TRADE_TIME ASC
 -- ORDER BY 最大收益率 DESC
-ORDER BY
-	最新日期收益率 DESC;
+ORDER BY 最新收益率 DESC;
 -- 每日收益率 DESC;
+;
 
 /**基金交易的最新净值-收益率**/
 SELECT
