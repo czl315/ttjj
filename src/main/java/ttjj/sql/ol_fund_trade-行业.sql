18c1511f34e96e43a552b928568a73183207410f
Administrator <czl315@163.com>
2020-10-08 23:43:43 +0800

    sql

---
+++ src/main/java/ttjj/sql/ol_fund_trade-行业.sql
@@ -0,0 +1,107 @@
+/**	分组-行业	*/
+SELECT ol_fund_trade.BIZ_TP
+	FROM ol_fund_trade
+	WHERE 1=1
+	AND ol_fund_trade.BIZ_TP NOT IN ('债券','货币')
+	GROUP BY ol_fund_trade.BIZ_TP
+;
+
+/**	分组-行业	*/
+SELECT 	ol_fund_trade.FD_INFO AS 大盘
+	,ol_fund_trade.CONFIRM_SHARE AS 买入份额
+	,ROUND(
+		(ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100,
+		4
+	) AS 最新收益率
+,ROUND((ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100/DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) ,4)
+		AS 每日收益率
+	,ol_fund_trade.CONFIRM_AMT AS 交易金额
+,DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) AS 持有天数
+	,ol_fund_trade.TRADE_TIME
+	,ROUND(ol_fund_trade.LAST_NET * ol_fund_trade.CONFIRM_SHARE ,2) AS '最新金额'
+	,ol_fund_trade.CONFIRM_NET AS confirmNet
+	,ol_fund_trade.LAST_NET lastNet
+	,ol_fund_trade.TYPE
+	,ol_fund_trade.BIZ_TP
+	FROM ol_fund_trade
+	WHERE 1=1
+	AND ol_fund_trade.TRADE_TIME>='2020-01-01 00:00:00'
+	AND ol_fund_trade.TYPE = '申购'
+	AND ol_fund_trade.BIZ_TP = '大盘'
+ORDER BY 最新收益率 DESC
+;
+
+/**	分组-行业	*/
+SELECT 	ol_fund_trade.FD_INFO AS 最新收益倒序
+	,ol_fund_trade.CONFIRM_SHARE AS 买入份额
+	,ROUND(
+		(ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100,
+		4
+	) AS 最新收益率
+,ROUND((ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100/DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) ,4)
+		AS 每日收益率
+	,ol_fund_trade.CONFIRM_AMT AS 交易金额
+,DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) AS 持有天数
+	,ol_fund_trade.TRADE_TIME
+	,ROUND(ol_fund_trade.LAST_NET * ol_fund_trade.CONFIRM_SHARE ,2) AS '最新金额'
+	,ol_fund_trade.CONFIRM_NET AS confirmNet
+	,ol_fund_trade.LAST_NET lastNet
+	,ol_fund_trade.TYPE
+	,ol_fund_trade.BIZ_TP
+	FROM ol_fund_trade
+	WHERE 1=1
+	AND ol_fund_trade.TRADE_TIME>='2020-01-01 00:00:00'
+	AND ol_fund_trade.TYPE = '申购'
+	AND ol_fund_trade.BIZ_TP = '中小盘'
+ORDER BY 最新收益率 DESC
+;
+
+/**	分组-行业	*/
+SELECT 	ol_fund_trade.FD_INFO AS 最新收益倒序
+	,ol_fund_trade.CONFIRM_SHARE AS 买入份额
+	,ROUND(
+		(ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100,
+		4
+	) AS 最新收益率
+,ROUND((ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100/DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) ,4)
+		AS 每日收益率
+	,ol_fund_trade.CONFIRM_AMT AS 交易金额
+,DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) AS 持有天数
+	,ol_fund_trade.TRADE_TIME
+	,ROUND(ol_fund_trade.LAST_NET * ol_fund_trade.CONFIRM_SHARE ,2) AS '最新金额'
+	,ol_fund_trade.CONFIRM_NET AS confirmNet
+	,ol_fund_trade.LAST_NET lastNet
+	,ol_fund_trade.TYPE
+	,ol_fund_trade.BIZ_TP
+	FROM ol_fund_trade
+	WHERE 1=1
+	AND ol_fund_trade.TRADE_TIME>='2020-01-01 00:00:00'
+	AND ol_fund_trade.TYPE = '申购'
+	AND ol_fund_trade.BIZ_TP = '金融'
+ORDER BY 最新收益率 DESC
+;
+
+/**	分组-行业	*/
+SELECT 	ol_fund_trade.FD_INFO AS 医药
+	,ol_fund_trade.CONFIRM_SHARE AS 买入份额
+	,ROUND(
+		(ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100,
+		4
+	) AS 最新收益率
+,ROUND((ol_fund_trade.LAST_NET - ol_fund_trade.CONFIRM_NET) * ol_fund_trade.CONFIRM_SHARE / ol_fund_trade.ORDER_AMT * 100/DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) ,4)
+		AS 每日收益率
+	,ol_fund_trade.CONFIRM_AMT AS 交易金额
+,DATEDIFF(NOW() ,ol_fund_trade.TRADE_TIME) AS 持有天数
+	,ol_fund_trade.TRADE_TIME
+	,ROUND(ol_fund_trade.LAST_NET * ol_fund_trade.CONFIRM_SHARE ,2) AS '最新金额'
+	,ol_fund_trade.CONFIRM_NET AS confirmNet
+	,ol_fund_trade.LAST_NET lastNet
+	,ol_fund_trade.TYPE
+	,ol_fund_trade.BIZ_TP
+	FROM ol_fund_trade
+	WHERE 1=1
+	AND ol_fund_trade.TRADE_TIME>='2020-01-01 00:00:00'
+	AND ol_fund_trade.TYPE = '申购'
+	AND ol_fund_trade.BIZ_TP = '医药'
+ORDER BY 最新收益率 DESC
+;
