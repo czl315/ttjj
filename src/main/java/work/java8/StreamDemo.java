a195a86fe4de00112759635af06f36f7f49c276e
chenzhilong9 <chenzhilong9@jd.com>
2020-11-17 10:18:15 +0800

    java:stream,mapDistinct

--- src/main/java/work/java8/StreamDemo.java
+++ src/main/java/work/java8/StreamDemo.java
@@ -1,13 +1,16 @@
 package work.java8;
 
 
+import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import ttjj.dto.FundTrade;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toCollection;
 
 /**
  * java8-stream
@@ -19,14 +22,58 @@ public class StreamDemo {
 //        test03SteamType();//生成流两种类型
 //        steam01();
 //        steamForEach();
-        map();
+//        map();
+        mapDistinct();
 
+    }
+
+    private static void mapDistinct() {
+        System.out.println("map 方法用于映射每个元素到对应的结果");
+        List<FundTrade> fundTradeList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            FundTrade fundTrade1 = new FundTrade();
+            fundTrade1.setFundInfo("测试" + i);
+            fundTradeList.add(fundTrade1);
+
+            FundTrade fundTrade2 = new FundTrade();
+            fundTrade2.setFundInfo("测试" + i);
+            fundTradeList.add(fundTrade2);
+        }
+
+        List<FundTrade> rs = new ArrayList<>();
+        List<FundTrade> newRs = new ArrayList<>();
+        rs = fundTradeList.stream().collect(
+                collectingAndThen(toCollection(() ->
+                        new TreeSet<>(Comparator.comparing(FundTrade::getFundInfo))), ArrayList::new
+                )
+        );
+
+        System.out.println("源数据：");
+        fundTradeList.stream().forEach(e -> {
+            System.out.println("hashCode:" + e + "," + JSON.toJSONString(e));
+        });
+        System.out.println();
+
+        System.out.println("目标数据:");
+        rs.stream().forEach(e -> {
+            System.out.println("hashCode:" + e + "," + JSON.toJSONString(e));
+            FundTrade fundTrade = new FundTrade();
+            BeanUtils.copyProperties(e, fundTrade);
+            newRs.add(fundTrade);
+        });
+        System.out.println();
+
+        System.out.println("目标数据线程安全对象：");
+        newRs.stream().forEach(e -> {
+            System.out.println("hashCode:" + e + "," + JSON.toJSONString(e));
+        });
+        System.out.println();
     }
 
     private static void map() {
         System.out.println("map 方法用于映射每个元素到对应的结果");
         List<Integer> integerList = Arrays.asList(1, 3, 5, 3, 5);
-        List<Integer> integerList2 = integerList.stream().map(integer -> integer*integer).collect(Collectors.toList());
+        List<Integer> integerList2 = integerList.stream().map(integer -> integer * integer).collect(Collectors.toList());
         integerList2.stream().forEach(integer -> {
             System.out.println(integer);
         });
