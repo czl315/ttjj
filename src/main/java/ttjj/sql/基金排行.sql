9f979d46531443c2dce9727aaa570e50957c6f1e
chenzhilong9 <chenzhilong9@jd.com>
2021-02-26 20:36:56 +0800

    sql

--- src/main/java/ttjj/sql/基金排行.sql
+++ src/main/java/ttjj/sql/基金排行.sql
@@ -1,11 +1,28 @@
-/**基金排行-今年	**/
+-- /**基金排行-今年+近一周	**/
+-- SELECT
+-- 	ROUND(ol_fund_rank.growthCurYear+ol_fund_rank.growth7,2) '今年+近一周'
+-- 	,ol_fund_rank.growthCurYear
+-- 	,ol_fund_rank.growth7
+-- 	,ol_fund_rank.*
+-- FROM
+-- 	`ol_fund_rank`
+-- WHERE 
+-- 	ol_fund_rank.periodTy = 'z'
+-- ORDER BY ROUND(ol_fund_rank.growthCurYear+ol_fund_rank.growth7,2) DESC
+-- LIMIT 0,
+--  100;
+
+/**基金排行-今年+近一周	**/
 SELECT
-	ol_fund_rank.growthCurYear-ol_fund_rank.growth7 '今年+近一周'
+	ROUND(ol_fund_rank.growthCurYear+ol_fund_rank.growth7,2) '今年+近一周'
 	,ol_fund_rank.growthCurYear
+	,ol_fund_rank.growth7
 	,ol_fund_rank.*
 FROM
 	`ol_fund_rank`
 WHERE 
 	ol_fund_rank.periodTy = 'jn'
+GROUP BY ol_fund_rank.fundCode,ol_fund_rank.periodDate
+ORDER BY ROUND(ol_fund_rank.growthCurYear+ol_fund_rank.growth7,2) DESC
 LIMIT 0,
  100;
