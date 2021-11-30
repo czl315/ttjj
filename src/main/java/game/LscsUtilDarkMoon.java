0f3c53a2a92d00b8e00c9347bcc80e13cbc08725
Administrator <czl315@163.com>
2021-11-30 14:09:08 +0800

    game

--- src/main/java/game/LscsUtilDarkMoon.java
+++ src/main/java/game/LscsUtilDarkMoon.java
@@ -18,8 +18,8 @@ public class LscsUtilDarkMoon {
         }
 
         String page = "";
-        String cardSet = "united-in-stormwind";//forged-in-the-barrens madness-at-the-darkmoon-faire  scholomance-academy
-        String cardTye = "1暴风城";//1贫瘠之地的锤炼  暗月马戏团   通灵学院
+        String cardSet = "fractured-in-alterac-valley";//united-in-stormwind   forged-in-the-barrens madness-at-the-darkmoon-faire  scholomance-academy
+        String cardTye = "0奥特兰克";//  1暴风城    1贫瘠之地的锤炼  暗月马戏团   通灵学院
 //        String cardClass = "neutral";
         String cardClass = "";
         for (int i = 0; i <= 0; i++) {
