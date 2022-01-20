9d1f5cc063e5bf84d88835ab816e3679b1413573
Administrator <czl315@163.com>
2022-01-20 17:16:59 +0800

    no Changelist

--- src/main/java/utils/ContentCookie.java
+++ src/main/java/utils/ContentCookie.java
@@ -8,6 +8,6 @@ package utils;
  */
 public class ContentCookie {
 
-    public static String COOKIE_DFCF = "eastmoney_txzq_zjzh=NTQwODIwMTc0NTY5fA%3D%3D; Yybdm=5408; Uid=fNUE23lwQOlyHFRjGcQYdA%3d%3d; Khmc=%e9%99%88%e5%bf%97%e9%be%99; st_si=14158131168588; st_asi=delete; st_pvi=68959131305862; st_sp=2021-04-02%2023%3A27%3A59; st_inirUrl=https%3A%2F%2Fjywg.18.cn%2FSearch%2FFundsFlow; st_sn=2; st_psi=20211123165716284-11923323313501-7781881241; mobileimei=780265e4-7197-418c-a7ec-fe9b4c061e2c; Uuid=f7fdf998c0094a8bac9150ea233273cd";
+    public static String COOKIE_DFCF = "";
 
 }
