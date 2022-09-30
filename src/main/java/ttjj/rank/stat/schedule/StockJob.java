e976d421044f94b6d8c9e807762d8996fc6066ae
Administrator <czl315@163.com>
2022-09-30 10:39:08 +0800

    股票：定时任务-股票-更新

---
+++ src/main/java/ttjj/rank/stat/schedule/StockJob.java
@@ -0,0 +1,33 @@
+package ttjj.rank.stat.schedule;
+
+import ttjj.rank.StockControl;
+import utils.DateUtil;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 定时任务
+ */
+public class StockJob {
+    public static void main(String[] args) {
+        String date = DateUtil.getToday(DateUtil.YYYY_MM_DD);
+        scheduleStock(date);//定时任务
+    }
+
+    /**
+     * 定时任务-股票-更新
+     *
+     * @param date 日期
+     */
+    private static void scheduleStock(String date) {
+        new ScheduledThreadPoolExecutor(1).scheduleAtFixedRate(() -> {
+            System.out.println();
+            System.out.println();
+            System.out.println("定时任务-股票-更新-beg:" + DateUtil.getCurDateStrAddDaysByFormat(DateUtil.YYYY_MM_DD_HH_MM_SS, 0));
+            StockControl.updateTodayStCom(date, 0);//更新股票
+            System.out.println("定时任务-股票-更新-end:");
+        }, 0, 15, TimeUnit.MINUTES);
+    }
+
+}
