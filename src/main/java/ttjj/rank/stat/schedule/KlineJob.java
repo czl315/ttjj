2d5cfadb2c878fc2ef46e02e31ba652bc9a657ec
Administrator <czl315@163.com>
2022-11-12 09:14:48 +0800

    工具:StopWatch用时分析

--- src/main/java/ttjj/rank/stat/schedule/KlineJob.java
+++ src/main/java/ttjj/rank/stat/schedule/KlineJob.java
@@ -1,5 +1,6 @@
 package ttjj.rank.stat.schedule;
 
+import org.springframework.util.StopWatch;
 import ttjj.rank.KlineControl;
 import utils.ContMapEtf;
 import utils.DateUtil;
@@ -24,12 +25,28 @@ public class KlineJob {
 
     private static void saveKlineEtfMianSchedule(String date) {
         new ScheduledThreadPoolExecutor(1).scheduleAtFixedRate(() -> {
+            long begTime = System.currentTimeMillis();
             System.out.println("定时任务-保存常用etf-beg:" + DateUtil.getCurDateStrAddDaysByFormat(DateUtil.YYYY_MM_DD_HH_MM_SS, 0));
             List<String> kltList_101_5 = Arrays.asList(KLT_5, KLT_15, KLT_30, KLT_60, KLT_101);
+            List<String> kltList_101_15 = Arrays.asList(KLT_15, KLT_30, KLT_60, KLT_101);
+            StopWatch sw = new StopWatch("保存K线");
+            sw.start("保存K线-指数");
             KlineControl.saveKlineAndMv(date, DB_RANK_BIZ_TYPE_ZS, kltList_101_5, KlineControl.handlerZqMap(date, DB_RANK_BIZ_TYPE_ZS));//
-            KlineControl.saveKlineAndMv(date, DB_RANK_BIZ_TYPE_ETF, kltList_101_5, KlineControl.handlerZqMap(date, DB_RANK_BIZ_TYPE_ETF));
+            sw.stop();
+            sw.start("保存K线-ETF");
+            KlineControl.saveKlineAndMv(date, DB_RANK_BIZ_TYPE_ETF, kltList_101_15, KlineControl.handlerZqMap(date, DB_RANK_BIZ_TYPE_ETF));
+            sw.stop();
+            sw.start("保存K线-板块");
+            KlineControl.saveKlineAndMv(date, DB_RANK_BIZ_TYPE_BAN_KUAI, kltList_101_15, KlineControl.handlerZqMap(date, DB_RANK_BIZ_TYPE_BAN_KUAI));//
+            sw.stop();
+//            sw.start("保存K线-概念");
+//            KlineControl.saveKlineAndMv(date, DB_RANK_BIZ_TYPE_GAI_NIAN, kltList_101_15, KlineControl.handlerZqMap(date, DB_RANK_BIZ_TYPE_GAI_NIAN));//
+//            sw.stop();
 //            KlineControl.saveKlineAndMv(date, DB_RANK_BIZ_TYPE_ETF, Arrays.asList(KLT_5, KLT_15, KLT_30, KLT_60, KLT_101), ContMapEtf.ETF_All);//保存常用etf
-            System.out.println("定时任务-保存常用etf-end:");
+            System.out.println(sw.prettyPrint());
+            System.out.println(sw.shortSummary());
+            System.out.println(sw.getTotalTimeMillis());
+            System.out.println("定时任务-保存K线-end:" + DateUtil.getCurDateStrAddDaysByFormat(DateUtil.YYYY_MM_DD_HH_MM_SS, 0) + "，用时：" + (System.currentTimeMillis() - begTime) / 1000);
         }, 0, 5, TimeUnit.MINUTES);
     }
 
