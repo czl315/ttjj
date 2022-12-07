895b79ab076c37d5c0268b6ecf4c134b39974920
Administrator <czl315@163.com>
2022-12-07 23:28:56 +0800

    game

--- src/main/java/game/LscsUtil.java
+++ src/main/java/game/LscsUtil.java
@@ -18,8 +18,8 @@ public class LscsUtil {
         }
 
         String page = "";
-        String cardSet = "murder-at-castle-nathria";//voyage-to-the-sunken-city：；united-in-stormwind   forged-in-the-barrens madness-at-the-darkmoon-faire  scholomance-academy fractured-in-alterac-valley
-        String cardTye = "2208纳堡悬案";//  1暴风城    1贫瘠之地的锤炼  暗月马戏团   通灵学院
+        String cardSet = "march-of-the-lich-king";//murder-at-castle-nathria;voyage-to-the-sunken-city：；united-in-stormwind   forged-in-the-barrens madness-at-the-darkmoon-faire  scholomance-academy fractured-in-alterac-valley
+        String cardTye = "2212巫妖王";//  1暴风城    1贫瘠之地的锤炼  暗月马戏团   通灵学院
 //        String cardClass = "neutral";
         String cardClass = "";
         for (int i = 0; i <= 0; i++) {
